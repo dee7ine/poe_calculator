cca3a1e44082cac459fad8104f4c87d5bca9798c
dee7ine <dee7ine@gmail.com>
2023-08-07 22:12:59 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+#!.\venv\Scripts\python.exe
+import aiohttp
+import asyncio
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+async def get_currency_json(league: str, type: str) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://poe.ninja/api/data/currencyoverview?league={league}&type={type}') as response:
+            with open('response.json', 'wb') as out:
+                async for chunk in response.content.iter_chunked(4096):
+                    out.write(chunk)
+                print('Succesfully parsed poe.ninja response')
+                    
+if __name__ == "__main__":      
+    asyncio.run(get_currency_json(league='Crucible', type='Currency'))
